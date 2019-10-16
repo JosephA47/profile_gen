7c66853ad0a35b188e5f21b40b3a4b4b9e34b9c9
Joseph <jandrade008@gmail.com>
2019-10-15 20:58:50 -0700

    added axios

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const questions = [
-  
-];
+//const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+const color = require("./generateHTML")
 
-function writeToFile(fileName, data) {
-    
-}
-
-function init() {
-
-init()};
+inquirer
+  .prompt({
+    message: "Enter your GitHub username",
+    name: "username"
+  })
+  .then(function({ username }) {
+    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+    axios.get(queryUrl).then(response => {
+      const repositoryNames = response.data.map(({name}) => name);
+      const nameStr = repositoryNames.join("\n");
+      console.log(nameStr);
+    })
+  });
