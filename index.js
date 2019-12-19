a398bcb189d465fa11e53f123c4802f94c83faee
Joseph <jandrade008@gmail.com>
2019-12-18 17:53:43 -0800

    added followers array

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-//const fs = require("fs");
+const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
-const color = require("./generateHTML")
+const HTML = require("./generateHTML")
 
 inquirer
-  .prompt({
+  .prompt([{
     message: "Enter your GitHub username",
     name: "username"
-  })
-  .then(function({ username }) {
+  },
+  {
+    message: "What is your favorite color",
+    name: "color"
+  }])
+  .then(function({ username, color }) {
     const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+    const queryUrl2 =  `https://api.github.com/users/${username}/followers`;
+
+    axios.get(queryUrl2).then(response => {
+      const followers = response.data.map(({number}) => number)
+
+      console.log(followers)
+    }),
+
     axios.get(queryUrl).then(response => {
       const repositoryNames = response.data.map(({name}) => name);
       const nameStr = repositoryNames.join("\n");
+
+      console.log("Your favorite color is " + color);
       console.log(nameStr);
     })
-  });
+  })
